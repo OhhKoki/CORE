44c35fce8ce68be9ec3137b6f3285adc5c95be6b
Terry <guochi.qin@outlook.com>
2024-03-14 13:14:17 +0800

    update

--- JUC/src/main/java/org/example/Test01.java
+++ JUC/src/main/java/org/example/Test01.java
@@ -1,4 +1,5 @@
-import org.junit.Test;
+package org.example;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
@@ -6,19 +7,25 @@ import java.util.concurrent.FutureTask;
  * 创建线程的三种方式
  */
 public class Test01 {
+    public static void main(String[] args) {
+        method3();
+    }
 
     /**
      * 方式一，直接通过 Thread 创建
      */
-    @Test
-    public void test01() {
+    private static void method1() {
         // 创建线程
         Thread thread = new Thread(){
             public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
                 System.out.println("通过 Thread 类创建的线程");
             }
         };
-
         // 启动线程
         thread.start();
     }
@@ -26,8 +33,7 @@ public class Test01 {
     /**
      * 方式二，Runnable 配合 Thread
      */
-    @Test
-    public void test02() {
+    private static void method2() {
         // 创建任务
         Runnable runnable = new Runnable() {
             @Override
@@ -43,8 +49,7 @@ public class Test01 {
     /**
      * 方式三，FutureTask 配合 Thread
      */
-    @Test
-    public void test03() throws Exception {
+    private static void method3() {
         // 创建任务
         Callable callable = new Callable() {
             @Override
@@ -55,13 +60,16 @@ public class Test01 {
         };
 
         // 将任务封装进 FutureTask 中，以便后续从该线程中获取处理结果
-        FutureTask<Integer> futureTask = new FutureTask(callable);
+        FutureTask<Integer> futureTask = new FutureTask<>(callable);
         // 创建并启动线程
         new Thread(futureTask).start();
 
         // 获取线程执行的结果
         // 主线程执行到该 get() 方法时，会进入阻塞状态，直到子线程执行完毕
-        System.out.println(futureTask.get());
+        try {
+            System.out.println(futureTask.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
