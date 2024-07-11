8e099ae46a9a94150e1d8b763783af1884b6c6bc
Terry <guochi.qin@outlook.com>
2024-07-11 22:11:35 +0800

    update

--- Functional/src/main/java/org/example/steam/StreamTest.java
+++ Functional/src/main/java/org/example/steam/StreamTest.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-public class TestStream {
+public class StreamTest {
 
     public static void main(String[] args) {
         test08();
